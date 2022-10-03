43d82a4675baccae312dd0b7b287828ee37489db
nikhilkalburgi <nikhilkalburgi19@gmail.com>
2022-10-03 18:39:53 +0530

    upgraded with AUTH TLS

--- test/client.js
+++ test/client.js
@@ -1,16 +1,19 @@
 //Import
-const FtpClient = require("ftp-for-node/client");
+const FtpClient = require("../client/index.js");
+const fs = require('fs');
 const ftp = new FtpClient();
 
 //Specify the available port for data-channel
 ftp.dataChannel.port = 3000;
 
 //For RETR
-ftp.localSite = "E:\\project\\ftp-for-node\\";
+ftp.localSite = "E:\\nodefiles\\folder name\\";
 
 //For Authorization - optional for anonymous
 ftp.user = "abc";
 ftp.password = "123";
+ftp.secureOptions.key = fs.readFileSync('key.pem');
+ftp.secureOptions.cert = fs.readFileSync('cert.pem');
 
 //"connect" - Listener
 ftp.on("connect",(code,msg)=>{
@@ -21,8 +24,20 @@ ftp.on("connect",(code,msg)=>{
     ftp.PWD((err,pwd)=>{
         console.log(err,pwd);
     })
-    ftp.LIST(null,(err,msg)=>{
+    ftp.PASV((err,msg)=>{
+        console.log(err,msg);
+    })
+    ftp.AUTH('tls',(err,msg)=>{
         console.log(err,msg)
+    })
+    ftp.NLST('./package.json',(err,msg)=>{
+        console.log(err,msg)
+    })
+    ftp.LIST('./package.json',(err,msg)=>{
+        console.log(err,msg)
+    })
+    ftp.STOR("package.json",(err,msg)=>{
+        console.log(err,msg);
     })
     ftp.QUIT((err,msg)=>{
         console.log(err,msg);
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 const FtpServer = require("../server");
+const fs = require('fs');
 const server = new FtpServer();
-server.defaultPWD = "E:/nodefiles";
-server.userDetails = [{name:"abc",password:"123",pwd:"E:\\nodefiles"}];
+server.defaultPWD = "E:/nodefiles/New";
+server.secureOptions.key = fs.readFileSync('key.pem');
+server.secureOptions.cert = fs.readFileSync('cert.pem');
+server.userDetails = [{name:"abc",password:"123",pwd:"E:/nodefiles"}];
 server.initiateFtpServer();
